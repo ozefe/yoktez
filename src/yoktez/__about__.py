71dd56d0462ea69e663abd548b6ef2b0f3a2d836
Efe Özyay <gh@efe.cv>
2026-05-21 00:56:53 +0300

    chore(release): v0.1.2
    Signed-off-by: Efe Özyay <gh@efe.cv>

--- src/yoktez/__about__.py
+++ src/yoktez/__about__.py
@@ -5,4 +5,4 @@ from importlib.metadata import PackageNotFoundError, version
 try:
     __version__ = version("yoktez")
 except PackageNotFoundError:
-    __version__ = "0.1.1+local"
+    __version__ = "0.1.2+local"
